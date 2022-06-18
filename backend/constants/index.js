39945c687bb6be9545275978900684cc57c6e66e
hrshrayank <hrshrayank77@gmail.com>
2022-06-19 01:39:58 +0530

    Commiting

---
+++ backend/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xa4F3A73bFb4Ce29935940b1931c16E488805DB30";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
